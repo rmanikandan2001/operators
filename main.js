bfb793e64baa270aa6a6b174e974a51bc808976f
rmanikandan2001 <rmanikandan6867@gmail.com>
2023-08-25 02:28:55 -0700

    add

---
+++ main.js
@@ -0,0 +1,128 @@
+//arithmetic operators start
+
+var a=33,b=2;
+console.log(a+b);
+
+console.log(a-b);
+
+console.log(a%b);   //Remainder
+
+console.log(a/b);   //Quatient
+
+console.log(a*b);   //
+
+console.log(a**b);  //10^5
+
+//arithhmetic operators end
+
+
+//pre-increment start
+
+i=10;
+console.log(++i);
+
+
+//pre-increment end
+
+//post-increment start
+
+i=10;
+
+console.log(i++);
+
+console.log(i++);
+
+//post-increment end
+
+//pre-decrement start
+
+i=100;
+
+console.log(--i);
+
+//pre-decrement end
+
+//post-decrement start
+
+i=50;
+
+console.log(i--);
+
+console.log(i--);
+
+//post-decrement end
+
+
+//concat start
+
+a="venkatesh",b=2;
+
+console.log(a+b);
+
+//concat end
+
+//comparision start
+
+a=10,b=7;
+
+console.log(a<b);
+
+console.log(a>b);
+
+console.log(a<=b);
+
+console.log(a>=b);
+
+console.log(a==b);
+
+console.log(a!=b);
+
+console.log(a===b);
+
+console.log(a!==b);    
+
+//comparision operator start end //
+
+
+//Assignment operator start //
+
+a=15;
+console.log(a+=5);  
+
+a=15;
+console.log(a-=9);  
+
+a=15;
+console.log(a/=2);
+
+a=15;
+console.log(a%=2); 
+
+a=15;
+console.log(a*=4);
+
+//Assignment operator end //
+
+// Logical operator start //
+
+a=10, b=5, c=7;
+
+console.log((a>b)&&(a>c));  
+
+a=10, b=5, c=12;
+
+console.log((a>b)&&(a>c));     //AND && Operator
+
+a=9, b=4, c=11;
+
+console.log((a>b)||(a>c));     // OR || Operator
+
+a=2, b=5, c=9;
+65
+console.log((a<b)!=(b>c));     //NOT != Operator "if output is false console is true"
+
+a=2, b=5, c=9;
+
+console.log((a<b)!=(b<c));     //NOT != Operator "if output is true console is false"
+
+// Logical operator end //
